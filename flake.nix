1e72b81fdc0d476a5b5d5d4c37b86eec44e5cada
o137 <git@o137.net>
2026-02-21 17:22:35 +0900

    feat: rust toolchain

--- flake.nix
+++ flake.nix
@@ -2,20 +2,48 @@
 
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
+
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs"
+    ];
   };
 
   outputs =
-    inputs@{ flake-parts, systems, ... }:
+    inputs@{
+      flake-parts,
+      nixpkgs,
+      systems,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import systems;
       perSystem =
-        { pkgs, ... }:
+        { pkgs, system, ... }:
         {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ inputs.fenix.overlays.default ];
+          };
+
           devShells.default = pkgs.mkShell {
             nativeBuildInputs = with pkgs; [
+              (pkgs.fenix.combine [
+                pkgs.fenix.complete.cargo
+                pkgs.fenix.complete.rustc
+                pkgs.fenix.complete.rust-src
+                pkgs.fenix.complete.clippy
+                pkgs.fenix.complete.rustfmt
+              ])
+              rust-analyzer
               pkg-config
             ];
 
